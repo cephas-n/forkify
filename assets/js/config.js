51e700f62aa28fb6f47ec605e6a120b3fb4f74d3
cephas-n <cephas.developer@gmail.com>
2022-07-27 02:14:03 +0300

    change src/ to assets/

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const API_KEY = '394df976-8d2a-41e2-8f67-244a30807cf4';
+export const API_KEY = '';
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
 export const MODEL_CLOSE_SEC = 3;
